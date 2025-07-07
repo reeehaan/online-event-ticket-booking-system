9fc256f817f06f1583eae67b3d2cec50dc4c174a
reeehaan <rehan221104@gmail.com>
2025-07-07 17:02:57 +0530

    feat: implement event creation API with validation

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,17 @@ const bodyParser = require("body-parser");
 require("./Database/mongodb.js");
 
 const user = require("./Routes/user-route");
+const organizer = require("./Routes/organizer-route.js");
 
 //body-parser
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
+//user
 app.use('/api/user',user);
+//organizer
+app.use('/api/org',organizer);
+
 
 
 const port = process.env.PORT || 3000;
