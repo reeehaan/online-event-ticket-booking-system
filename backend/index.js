06d9724ce145aed486013a9a67d2abcba3e7498e
reeehaan <rehan221104@gmail.com>
2025-07-10 01:19:40 +0530

    fix : network payload limit

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,26 @@ require("./Database/mongodb.js");
 
 const user = require("./Routes/user-route");
 const organizer = require("./Routes/organizer-route.js");
-
+const attendee = require("./Routes/attendee-route.js")
 //body-parser
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.use(express.json({ 
+    limit: '50mb' 
+}));
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ 
+    limit: '50mb', 
+    extended: true 
+}));
 app.use(cors());
 //user
 app.use('/api/user',user);
 //organizer
 app.use('/api/org',organizer);
+
+app.use('/api/attendee',attendee);
 
 
 
