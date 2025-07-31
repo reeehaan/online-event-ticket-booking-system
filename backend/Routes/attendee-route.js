479c1176b6686d6617d4bfd222a5dc1bc0a1dfbf
reeehaan <rehan221104@gmail.com>
2025-07-31 21:00:43 +0530

    feat : add routes for get booking history & booking stats

--- backend/Routes/attendee-route.js
+++ backend/Routes/attendee-route.js
@@ -3,8 +3,12 @@ const router = require('express').Router();
 const { verifyToken , verifyAttendee} = require('../Middleware/auth');
 const Attendee = require('../Service/AttendeeService');
 
-router.get('/get-all-event', verifyToken, verifyAttendee,  Attendee.getAllEvents);
+router.get('/get-all-event', verifyToken,  Attendee.getAllEvents);
 router.get('/by-category', verifyToken, Attendee.getEventsByCategory);
 router.get('/recent-events', verifyToken, Attendee.getRecentEvents);
+
+
+router.get('/booking-history', verifyToken, Attendee.getBookingHistory);
+router.get('/booking-stats', verifyToken, Attendee.getBookingStats);
 
 module.exports = router;
